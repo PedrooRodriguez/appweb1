05a7e7e93a11bc6249a6cd755e509537f731ae94
Paco <pedropandrox@gmail.com>
2025-04-23 11:19:49 -0500

    carga de clientes

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using appweb1.Models;
+namespace appweb1.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+
+        public DbSet<Cliente> Clientes{get;set;}
+
+
+    }
+}
